b54b0e928d9ce810fe7006620563cdef0989a826
张君卓 <my_tac@126.com>
2017-07-31 17:07:23 +0800

    添加页标题

--- src/pages/AddPage.js
+++ src/pages/AddPage.js
@@ -2,14 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import BackButton from '../components/Icon/LeftArrow';
+import NormalText from '../components/Content/NormalText';
 import Form from '../containers/Form';
 
 import '../less/addPage.less';
+
+const textStyle = {
+  color: '#fff',
+  fontSize: '32px',
+  margin: '50px auto',
+  textAlign: 'center',
+};
 
 export default function AddPage({ changeTab }) {
   return (
     <div className="add-page">
       <BackButton onClick={changeTab} />
+      <NormalText text="Add new things" style={textStyle} />
       <Form />
     </div>
   );
