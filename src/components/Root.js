3c70ce6f9961f1053d2a898893e33243e7555bbf
张君卓 <my_tac@126.com>
2017-07-31 09:54:11 +0800

    修改root

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,19 +1,12 @@
 import React from 'react';
 
-import TopBar from './ListTopBar/TopBar'
-import List from '../containers/List'
-import CrossButton from './Button/CrossButton'
-import BottomBar from './BottomBar'
-
+import ListPage from '../containers/ListPage'
 import '../less/index.less'
 
 export default function Root(){
     return(
         <div>
-            <TopBar/>
-            <List/>
-            <CrossButton/>
-            <BottomBar/>
+            <ListPage/>
         </div>
     )
 }
--- src/components/addPage/index.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from 'react'
-
-export default class AddPage extends Component{
-    render(){
-        return(
-            <div>AddPage</div>
-        )
-    }
-}
