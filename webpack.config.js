b84ed5c5d523ea82a9500b56943d16e99b1e4a24
张君卓 <my_tac@126.com>
2017-08-07 17:51:53 +0800

    webpack run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'todo-app',
+      template: 'template.html',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
