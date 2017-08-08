f1d9e3366aec260ff8f173b4f1d2300517f398f6
张君卓 <my_tac@126.com>
2017-08-08 18:05:02 +0800

    添加请求action type以及action reducer

--- src/action/index.js
+++ src/action/index.js
@@ -4,6 +4,10 @@ export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const CHANGE_TAB = 'CHANGE_TAB';
 export const DELETE_TODO = 'DELETE_TODO';
 
+export const FETCH_POSTS_REQUEST = 'FETCH_POSTS_REQUEST';
+export const FETCH_POSTS_FAILURE = 'FETCH_POSTS_FAILURE';
+export const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS ';
 // action constants
 export const pageFilters = {
   GO_LIST: 'GO_LIST',
@@ -11,7 +15,6 @@ export const pageFilters = {
 };
 
 // action creators
-// todo:尝试下传索引和deleteTodo一起封装成一个action
 // newTodo 是一个对象
 export function addTodo(newTodo) {
   return { type: ADD_TODO, newTodo };
@@ -27,4 +30,29 @@ export function changeTab(filter) {
 
 export function deleteTodo(index) {
   return { type: DELETE_TODO, index };
+}
+
+export function fetchPostsRequest() {
+  return { type: FETCH_POSTS_REQUEST };
+}
+export function fetchPostsFailure() {
+  return { type: FETCH_POSTS_FAILURE, error: 'Oops!' };
+}
+export function fetchPostsSuccess(response) {
+  return { type: FETCH_POSTS_SUCCESS, response };
+}
+export function receivePosts(response, json) {
+  return { type: RECEIVE_POSTS, posts: JSON.parse(json) };
+}
+
+// thunk
+export function fetchPosts() {
+  return (dispatch) => {
+    dispatch(fetchPostsRequest());
+    return setTimeout(() => {
+      dispatch(fetchPostsSuccess());
+      return { name: 'zjz', age: '10' };
+    }
+      , 500);
+  };
 }
